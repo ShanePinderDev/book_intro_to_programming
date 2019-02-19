6c3dda544e9e14aaddac97a050cabf15667fbc48
Shane Pinder <pshanepinder@gmail.com>
2019-02-19 07:53:22 -0500

    Commit Exercise 2 of The Basics Chapter

---
+++ the_basics/ex2.rb
@@ -0,0 +1,15 @@
+thousands = 5618 / 1000
+
+hundreds = 5618 % 1000 / 100
+
+tens = 5618 % 100 / 10
+
+ones = 5618 % 10 / 1
+
+puts "thousands: #{thousands}"
+
+puts "hundreds: #{hundreds}"
+
+puts "tens: #{tens}"
+
+puts "ones: #{ones}"
