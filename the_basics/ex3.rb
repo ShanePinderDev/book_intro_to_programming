ebe89c4b119963e8bf43aa73bacf9d67c3952869
Shane Pinder <pshanepinder@gmail.com>
2019-02-20 07:19:46 -0500

    Commit of exercises 3 to 6 of The Basics chapter.

---
+++ the_basics/ex3.rb
@@ -0,0 +1,3 @@
+movie_dates = {:movie1 => 1967, :movie2 => 1970, :movie3 => 1973, :movie4 => 1976}
+
+puts movie_dates[:movie1]
---
+++ the_basics/ex4.rb
@@ -0,0 +1,3 @@
+movie_dates = [1967, 1970, 1973, 1976]
+
+puts movie_dates[0]
