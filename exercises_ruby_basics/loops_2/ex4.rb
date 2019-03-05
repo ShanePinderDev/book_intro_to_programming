10374c277383802e5acea531b9cbba35b336286e
Shane Pinder <pshanepinder@gmail.com>
2019-03-05 07:39:05 -0500

    Commit exercises 4 and 5 of loops_2

---
+++ exercises_ruby_basics/loops_2/ex4.rb
@@ -0,0 +1,26 @@
+#mysolution
+=begin
+loop do 
+  puts 'What does 2 + 2 equal?'
+  answer = gets.chomp.to_i
+  if answer == 4
+    puts "That's correct!"
+  break
+  else
+    puts "Wrong answer. Try again!"
+  end
+end
+=end
+
+#givensolution
+
+loop do 
+  puts 'What does 2 + 2 equal?'
+  answer = gets.chomp.to_i
+  if answer == 4
+    puts "That's correct!"
+  break
+  end
+    puts "Wrong answer. Try again!"
+  
+end
---
+++ exercises_ruby_basics/loops_2/ex5.rb
@@ -0,0 +1,11 @@
+# my solution and given solution!
+numbers = []
+
+loop do
+  puts 'Enter any number:'
+  input = gets.chomp.to_i
+  numbers.push(input)
+  break if numbers.size == 5
+end
+
+puts numbers
